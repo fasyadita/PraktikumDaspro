21835e82bc001524a60e9abf65848a23d86cc8da
fasyadita <fasyadita1@gmail.com>
2023-10-11 12:08:11 +0700

    selese ye

--- Praktikum06/KabisatNested12.java
+++ Praktikum06/KabisatNested12.java
@@ -9,16 +9,21 @@ Scanner sc = new Scanner (System.in);
     tahun = sc.nextInt();
 
     if ((tahun %4)==0) {
-        if ((tahun % 100) !=0)
-        System.out.println("Tahun Kabisat");
+        if ((tahun % 100) !=0){
+            System.out.println("Tahun Kabisat"); 
+        } 
         else {
-        System.out.println("Bukan Tahun Kabisat");
+             if ((tahun % 400) ==0) {
+                System.out.println("Tahun kabisat");
+ 
+            } else {
+                System.out.println("Bukan Tahun Kabisat");
+            }
         }
     } else {
-        System.out.println("Bukan Tahun Kabisat");
+        System.out.println("bukan tahun kabisat");
+       
     }
-
-
 
 }
 }
