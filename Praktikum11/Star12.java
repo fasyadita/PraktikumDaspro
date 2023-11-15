f4cf33cc18adc8ead6c5b631d932be54a5be82d4
fasyadita <fasyadita1@gmail.com>
2023-11-15 11:06:07 +0700

    persegi nested loop

--- Praktikum11/Star12.java
+++ Praktikum11/Star12.java
@@ -6,7 +6,7 @@ public class Star12{
         System.out.print("Masukkan Jumlah *: ");
         int n = sc.nextInt();
 
-        for (int i = 1 ; i <= n ; i++){
+        for (int i = 0 ; i <= n ; i++){
             System.out.print("*");
         }
 
