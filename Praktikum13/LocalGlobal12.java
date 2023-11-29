ecda66d32aa79a056d7a996ffca6b3f5d618888a
fasyadita <fasyadita1@gmail.com>
2023-11-29 13:43:53 +0700

    variabel lokal dan global

---
+++ Praktikum13/LocalGlobal12.java
@@ -0,0 +1,25 @@
+public class LocalGlobal12 {
+    static int a = 10;
+
+    public static void main(String[] args) {
+        int b = 10;
+        int c = 17;
+        a= a+8;
+
+        System.out.println("Nilai a: " +a);
+        System.out.println("Nilai b: " +b);
+        System.out.println("Nilai c: " +c);
+
+        display();
+    }
+
+    public static void display() {
+        int b = 20;
+        int c = 5;
+        a= a+12;
+        
+        System.out.println("Nilai a: " +a);
+        System.out.println("Nilai b: " +b);
+        System.out.println("Nilai c: " +c);
+    }
+}
