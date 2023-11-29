c99364f089f979c90a78e64a1e090298057fc435
fasyadita <fasyadita1@gmail.com>
2023-11-29 10:49:29 +0700

    fungsi 1

---
+++ Praktikum13/Welcome12.java
@@ -0,0 +1,9 @@
+public class Welcome12 {
+    public static void main(String[] args) {
+        displayWelcomeMessage();
+    }
+    
+    public static void displayWelcomeMessage(){
+    System.out.println("Selamat datangdi Politeknik Negeri Malang");
+    }
+}
