5b32d42815e37bd177b8b21c23431ee9a53607a1
fasyadita <fasyadita1@gmail.com>
2023-11-29 13:25:21 +0700

    args sebagai scanner langsung

---
+++ Praktikum13/CobaArgs12.java
@@ -0,0 +1,10 @@
+public class CobaArgs12 {
+    public static void main(String[] args) {
+        System.out.println("Ha i" + args[0]);
+        System.out.println("Selamat datang di " + args[1]);
+
+        // for (int i = 0; i < args.length; i++) {
+        //     System.out.println(args[i]);
+        // }
+    }
+}
